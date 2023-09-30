b4d44fa5aa901787388aaa111df59fb4502cab78
Vitasz <bogomiavitalii@mail.ru>
2023-09-30 16:57:19 +0300

    add sessions

--- Android/app/src/main/java/com/monke/begit/ActivityDetector.kt
+++
@@ -1,5 +0,0 @@
-package com.monke.begit
-
-class ActivityDetector {
-
-}
--- Android/app/src/main/java/com/monke/begit/MainActivity.kt
+++ Android/app/src/main/java/com/monke/begit/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.monke.begit
 
+import android.Manifest
 import android.Manifest.permission.ACTIVITY_RECOGNITION
 import android.Manifest.permission.CAMERA
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,9 +12,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.SyncStateContract
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.uwasting.data.LocalDateDeserializer
@@ -21,20 +25,31 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.Fitness.getSessionsClient
+import com.google.android.gms.fitness.FitnessActivities
 import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataPoint
+import com.google.android.gms.fitness.data.DataSet
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.data.Field
 import com.google.android.gms.fitness.request.DataReadRequest
 import com.google.android.gms.fitness.request.OnDataPointListener
+import com.google.android.gms.fitness.request.SessionReadRequest
 import com.google.gson.GsonBuilder
 import com.monke.begit.data.remote.Constants
+import com.monke.begit.data.remote.GoogleFitAPI
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.concurrent.TimeUnit
 
 
@@ -42,6 +57,11 @@ private const val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1001
 class MainActivity : AppCompatActivity() {
     private lateinit var api: API
     private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var button2: Button
+    private lateinit var googleFitAPI: GoogleFitAPI
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //configureRetrofit()
@@ -49,179 +69,41 @@ class MainActivity : AppCompatActivity() {
         //api.getActivities(1)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
         textView.text = "HI"
-        setupGoogleFit()
 
-    }
 
-    private lateinit var fitnessOptions: FitnessOptions
-    private lateinit var googleSignInClient: GoogleSignInClient
 
-    private fun setupGoogleFit() {
-        val cc = ContextCompat.checkSelfPermission(this, ACTIVITY_RECOGNITION)
-        if (cc
-            != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+            != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED ) {
             ActivityCompat.requestPermissions(this,
-                arrayOf(ACTIVITY_RECOGNITION),
+                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION,
+Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
                 1)
             // Permission is not granted
         }
 
-        fitnessOptions = FitnessOptions.builder()
-            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-            .build()
-
-        val account = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
-        googleSignInClient =
-            GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN)
-
-        if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
-            GoogleSignIn.requestPermissions(
-                this,
-                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                account,
-                fitnessOptions
-            )
-        } else {
-            val readRequest = DataReadRequest.Builder()
-                .read(DataType.TYPE_STEP_COUNT_DELTA)
-                .setTimeRange(
-                    System.currentTimeMillis() - 100000,
-                    System.currentTimeMillis(),
-                    TimeUnit.MILLISECONDS
-                )
-                .build()
-            account.let {
-                Fitness.getHistoryClient(this, it)
-                    .readData(readRequest)
-                    .addOnSuccessListener { response ->
-                        val dataSet = response.getDataSet(DataType.TYPE_STEP_COUNT_DELTA)
-                        val totalSteps =
-                            if (dataSet.isEmpty) 0 else dataSet.getDataPoints().sumBy {
-                                it.getValue(
-                                    Field.FIELD_STEPS
-                                ).asInt()
-                            }
-
-                        // Print the total number of steps in a view
-                        textView.text = "Total Steps: $totalSteps"
-                    }
-                    .addOnFailureListener { e ->
-                        Log.e("ERROR", "There was a problem reading step count data.", e)
-                    }
-            }
-        }
 
 
-        /*
-        val listener = OnDataPointListener { dataPoint ->
-            for (field in dataPoint.dataType.fields) {
-                val value = dataPoint.getValue(field)
-                val stepCount = value.asInt()
-                Log.d("STEPS:", stepCount.toString())
-            }
-        }
-
-        Fitness.getHistoryClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-            .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
-            .addOnSuccessListener { dataSet ->
-                for (dataPoint in dataSet.dataPoints) {
-                    for (field in dataPoint.dataType.fields) {
-                        val value = dataPoint.getValue(field)
-                        val stepCount = value.asInt()
-                        // Handle initial step count
-                    }
-                }
-            }
-
-        Fitness.getRecordingClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-            .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
-            .addOnSuccessListener {
-                // Successfully subscribed to step count updates
-            }*/
+        googleFitAPI = GoogleFitAPI(this)
+        //x.sendRequest(DataType.TYPE_STEP_COUNT_DELTA, LocalDateTime.now().atZone(System),0,TimeUnit.MILLISECONDS)
+        googleFitAPI.setupGoogleFit()
+        button.setOnClickListener { startActivity() }
+        button2.setOnClickListener { endActivity() }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                fitnessOptions = FitnessOptions.builder()
-                    .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                    .build()
-
-                val account = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
-                googleSignInClient =
-                    GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN)
-
-                if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
-                    GoogleSignIn.requestPermissions(
-                        this,
-                        GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                        account,
-                        fitnessOptions
-                    )
-                } else {
-                    val readRequest = DataReadRequest.Builder()
-                        .read(DataType.TYPE_STEP_COUNT_DELTA)
-                        .setTimeRange(
-                            System.currentTimeMillis() - 100000,
-                            System.currentTimeMillis(),
-                            TimeUnit.MILLISECONDS
-                        )
-                        .build()
-                    account.let {
-                        Fitness.getHistoryClient(this, it)
-                            .readData(readRequest)
-                            .addOnSuccessListener { response ->
-                                val dataSet = response.getDataSet(DataType.TYPE_STEP_COUNT_DELTA)
-                                val totalSteps =
-                                    if (dataSet.isEmpty) 0 else dataSet.getDataPoints().sumBy {
-                                        it.getValue(
-                                            Field.FIELD_STEPS
-                                        ).asInt()
-                                    }
-
-                                // Print the total number of steps in a view
-                                textView.text = "Total Steps: $totalSteps"
-                            }
-                            .addOnFailureListener { e ->
-                                Log.e("ERROR", "There was a problem reading step count data.", e)
-                            }
-                    }
-                }
-            }
-        }
+    private fun startActivity(){
+        googleFitAPI.checkDevicesAndStartSession(Constants.FITNESS_ACTIVITIES[2], Constants.DATA_TYPES[2])
     }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                subscribeToStepCountUpdates()
-            } else {
-                Log.e("FUCK", resultCode.toString())
-            }
-        }
+    private fun endActivity(){
+        googleFitAPI.EndSession()
     }
-
-    private fun subscribeToStepCountUpdates() {
-        val client = Fitness.getRecordingClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-        client.subscribe(DataType.TYPE_STEP_COUNT_DELTA)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // Successfully subscribed to step count updates
-                } else {
-                    // Handle subscription failure
-                }
-            }
-    }
-
-    // Натсройка подключения к серверу
+    // Настройка подключения к серверу
     private fun configureRetrofit() {
         val gson = GsonBuilder().registerTypeAdapter(LocalDate::class.java, LocalDateDeserializer()).create()
         val httpLoggingInterceptor = HttpLoggingInterceptor()
